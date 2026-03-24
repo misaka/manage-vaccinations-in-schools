9db889af1a31999cb8817d93b44829e166c7ba51
Thomas Leese <thomas@leese.io>
2026-03-24 12:48:17 +0000

    Rename `school` location type
    This renames the type to `gias_school` to make the distinction between
    it and the `generic_school` location type clearer. Specifically, the two
    are location types are similar, but are not subsets of each other.
    
    Jira-Issue: MAV-5556

--- spec/factories/patients.rb
+++ spec/factories/patients.rb
@@ -117,7 +117,8 @@ FactoryBot.define do
     registration { Faker::Alphanumeric.alpha(number: 2).upcase }
     registration_academic_year { academic_year if registration.present? }
 
-    school { session.location if session&.location&.school? }
+    # TODO: school?
+    school { session.location if session&.location&.gias_school? }
 
     address_line_1 { Faker::Address.street_address }
     address_line_2 { Faker::Address.secondary_address }
